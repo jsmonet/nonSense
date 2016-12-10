1a786fe6e1c0b6a7fa8088d527b4f8946764deb5
joe franco <jsmonet@gmail.com>
2016-12-10 17:10:56 +0000

    initial commit

---
+++ display_temp_fahr.py
@@ -0,0 +1,9 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+sense.set_rotation(270)
+def tofahr(celsius):
+    return( (celsius/5*9)+32 )
+
+longtemp = tofahr(sense.get_temperature())
+temp = "{0:.2f}".format(longtemp) 
+sense.show_message(temp, text_colour=[0, 255, 0])
