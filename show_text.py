78a557c15ddd739c9030880649bb6df496e479f7
Joe Franco <jsmonet@senjougahara.local>
2016-12-10 19:45:47 -0800

    throwing a little fun into this

--- show_text.py
+++ show_text.py
@@ -3,5 +3,13 @@ import sys
 import nltk
 sense = SenseHat()
 words = nltk.word_tokenize(sys.argv[1])
+rotayshun = sys.argv[2]
+if any(nu in rotayshun for nu in ('0', '90', '180', '270')):
+    sense.set_rotation(rotayshun)
+elif rotayshun is None:
+    sense.set_rotation(180)
+else:
+    print "Figure your shit out, jerky"
+    exit 1
 for word in words:
     sense.show_message(word)
