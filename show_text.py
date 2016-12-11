e30c524b568f2b376902b783cea914b540408de7
joe franco <jsmonet@gmail.com>
2016-12-11 03:54:27 +0000

    I really do not understand why I did not go with the easy method first

--- show_text.py
+++ show_text.py
@@ -3,13 +3,12 @@ import sys
 import nltk
 sense = SenseHat()
 words = nltk.word_tokenize(sys.argv[1])
-rotayshun = sys.argv[2]
-if any(nu in rotayshun for nu in ('0', '90', '180', '270')):
+rotayshun = int(sys.argv[2])
+if rotayshun == 0 or rotayshun % 90 == 0:
     sense.set_rotation(rotayshun)
 elif rotayshun is None:
     sense.set_rotation(180)
 else:
     print "Figure your shit out, jerky"
-    exit 1
 for word in words:
     sense.show_message(word)
