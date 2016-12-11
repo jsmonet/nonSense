7ac6d978a8dc2d0093b35c68afaeb11dea73ddcb
joe franco <jsmonet@gmail.com>
2016-12-11 03:34:28 +0000

    I am too dumb to multi-var increment right now

--- show_text.py
+++ show_text.py
@@ -4,9 +4,4 @@ import nltk
 sense = SenseHat()
 words = nltk.word_tokenize(sys.argv[1])
 for word in words:
-	a = [0, 0, 255]
-	b = [0, 255, 0]
-	c = [255, 0, 0]
-	for ax, bx, cx in zip(a, b, c)
-	
-		sense.show_message(word, text_colour=[ax, bx, cx])
+    sense.show_message(word)
