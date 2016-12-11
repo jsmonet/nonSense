5241506be473690203cca2fd0f6cf6de96fe90d7
Joe Franco <jsmonet@gmail.com>
2016-12-11 15:00:09 -0800

    casing out with if elif for some reactive text coloring

--- temp_fahr_and_rh.py
+++ temp_fahr_and_rh.py
@@ -7,6 +7,14 @@ sense.set_rotation(180)
 # this makes the output readable for me. I'm setting it up here
 # because this file is tiny and containment isn't a big deal
 
+# colors, or colours
+red = (255, 0, 0)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+aqua = (0, 255, 255)
+white = (255, 255, 255)
+
 # SenseHat returns celsius, the temp scale of oppression.
 # Let's convert it to glorious units of confusing freedom
 
@@ -21,7 +29,32 @@ temp = "{0:.1f}".format(longtemp) # I like the shorter format
 rh = "{0:.1f}".format(longrh)
 # And now we'll write it to the LED panel on the SenseHat
 # in green because I like green.
-sense.show_message(temp + "F & ", text_colour=[0, 255, 0])
-sense.show_message(rh + "%rH", text_colour=[0, 0, 255])
+if temp < 65.0:
+    tempcolour = blue
+elif temp >= 65.0 and temp < 70.0:
+    tempcolour = aqua
+elif temp >= 70.0 and temp < 76.0:
+    tempcolour = green
+elif temp >= 76.0 and temp < 80.0:
+    tempcolour = yellow
+else:
+    tempcolour = red
+
+if rh < 15:
+    rhcolour = red
+elif rh >= 15 and rh < 20:
+    rhcolour = yellow
+elif rh >= 20 and rh < 30:
+    rhcolour = aqua
+elif rh >= 30 and rh < 50:
+    rhcolour = green
+elif rh >= 50 and rh < 70:
+    rhcolour = yellow
+else:
+    rhcolour = red
+
+sense.show_message(temp + "F ", text_colour=tempcolour)
+sense.show_message("& ", text_colour=white)
+sense.show_message(rh + "%rH", text_colour=rhcolour)
 
 # COMMENTS!
