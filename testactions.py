f7fc7c00f609ec8892ae6a05152a5fdcbf4458e2
joe franco <jsmonet@gmail.com>
2016-12-14 17:44:06 -0800

    raw input to input because clutter and old, very bad habits

--- testactions.py
+++ testactions.py
@@ -8,13 +8,13 @@ print "Please select an action from the list:" \
     "1: Temp" \
     "2: rH" \
     "3: Temp & rH"
-choice = raw_input("Please enter 1, 2, or 3: ")
+choice = input("Please enter 1, 2, or 3: ")
 
-if int(choice) == 1:
+if choice == 1:
     astroact.showtemp()
-elif int(choice) == 2:
+elif choice == 2:
     astroact.showrh()
-elif int(choice) == 3:
+elif choice == 3:
     astroact.showboth()
 else:
     print "you entered an invalid number"
