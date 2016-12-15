bdd3e92ceac73d8fcb1a4581dddfb3dce3c5a165
joe franco <jsmonet@gmail.com>
2016-12-15 11:49:23 -0800

    just laid this down to use with slacktee while the entire HSC campus is offline

---
+++ cli_temp_rh.py
@@ -0,0 +1,17 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+import sys
+# Start every SenseHat file with this because SenseHat.
+
+def tofahr(celsius):
+    return( (celsius/5*9)+32 )
+
+longtemp = tofahr(sense.get_temperature())
+longrh = sense.get_humidity()
+# And then trim the output to 2 sig figs.
+temp = "{0:.1f}".format(longtemp) # I like the shorter format
+rh = "{0:.1f}".format(longrh)
+
+print "The temp is " + temp + " and the humidity is " + rh 
+
+# COMMENTS!
