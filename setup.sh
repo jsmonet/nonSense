12b701d12800cc7264b7c4213537307b9c1c87b4
Joe Franco <jsmonet@gmail.com>
2016-12-18 00:42:25 -0800

    cleanup

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -e "setting up basic packages"
-if [ -z `which pip` ]; then sudo apt-get install -y python-pip; else echo "already installed"; fi
-
-for pippkg in pydns validate_email; do if pip list | grep ${pippkg}; then echo "$pippkg installed"; else sudo pip install ${pippkg}; fi; done
-
-if [ -z `apt -qq list sense-hat`]; then sudo apt-get install -y sense-hat; else echo "sense-hat already installed"; fi
