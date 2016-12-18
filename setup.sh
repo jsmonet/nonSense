3df929b1aea595b3ee7d03f64dbb75db9ff5b6c8
Joe Franco <jsmonet@gmail.com>
2016-12-18 00:26:16 -0800

    never hit save. now i have hit save. bleh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e "setting up basic packages"
+if [ -z `which pip` ]; then sudo apt-get install -y python-pip; else echo "already installed"; fi
+
+for pippkg in pydns validate_email; do if pip list | grep ${pippkg}; then echo "$pippkg installed"; else sudo pip install ${pippkg}; fi
+
+if [ -z `apt -qq list sense-hat`]; then sudo apt-get install -y sense-hat; else echo "sense-hat already installed"
